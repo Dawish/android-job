d96a33536706699817001190ba3ccabeed6e13ff
danxx <danxx@yanhuamedia.com>
2018-04-18 13:41:19 +0800

    检测数据库的支持

--- demo/src/main/java/com/evernote/android/job/demo/App.java
+++ demo/src/main/java/com/evernote/android/job/demo/App.java
@@ -1,9 +1,13 @@
 package com.evernote.android.job.demo;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.evernote.android.job.JobManager;
+import com.evernote.android.job.JobRequest;
 import com.facebook.stetho.Stetho;
+
+import java.util.Set;
 
 /**
  * @author rwondratschek
@@ -16,6 +20,11 @@ public class App extends Application {
         Stetho.initializeWithDefaults(this);
 
         JobManager.create(this).addJobCreator(new DemoJobCreator());
+
+        Set<JobRequest> jobRequests = JobManager.instance().getAllJobRequests();
+        if(jobRequests != null){
+            Log.d("danxx","dataBase Size :" + jobRequests.size());
+        }
     }
 }
 
